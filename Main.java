63f48bd1fa5da5880a8eb9706d4cee31c18bd675
Emmanuel Slh <emmanuel.salameh@edu.ece.fr>
2022-05-04 11:40:08 +0200

    First commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) 
+  {
+ String txt1 = "petit test";
+ String txt2 = "autre test";
+ System.out.println(txt1.toUpperCase());
+ System.out.println(txt1.toLowerCase());
+ System.out.println(txt1.length());
+ System.out.println(txt1.indexOf("s"));
+ System.out.println(txt1 + "  tema le big espace  " + txt2);
+
+
+
+  }
+}
